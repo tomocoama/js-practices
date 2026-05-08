157f593400f9eda8f6dd7a26e74b65ba5bad2325
tomocoama <k.tomoco1128@gmail.com>
2026-05-08 14:11:17 +0900

    Prettier Playground でコード再整形

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,14 +3,11 @@
 for (let num = 1; num <= 20; num++) {
   if (num % 3 === 0 && num % 5 === 0) {
     console.log("FizzBuzz");
-  }
-  else if (num % 3 === 0) {
+  } else if (num % 3 === 0) {
     console.log("Fizz");
-  }
-  else if (num % 5 === 0) {
+  } else if (num % 5 === 0) {
     console.log("Buzz");
-  }
-  else {
+  } else {
     console.log(String(num));
   }
 }
