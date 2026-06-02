27f9f8062177923a2020dfb969f6f0d1539520d8
tomocoama <k.tomoco1128@gmail.com>
2026-06-02 15:55:27 +0900

    曜日と日付を表示

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,3 +1,22 @@
-const today = new Date();
+const year = Number(process.argv[2]);
+const month = Number(process.argv[3]);
 
-console.log(today);
+const firstDay = new Date(year, month - 1, 1);
+const lastDay = new Date(year, month, 0);
+const weeks = ["日", "月", "火", "水", "木", "金", "土"];
+
+console.log(weeks.join(" "));
+
+let dates = " ";
+for (let i = 0; i < firstDay.getDay(); i++) {
+  dates += "   ";
+}
+for (let date = 1; date <= lastDay.getDate(); date++) {
+  dates += `${date} `;
+  if ((firstDay.getDay() + date) % 7 === 0) {
+    dates += "\n";
+  }
+}
+console.log(dates);
+
+console.log(firstDay.getDay());
