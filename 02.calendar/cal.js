91f389ae55a893ee1a7576a7a69d747ff4b1fd25
tomocoama <k.tomoco1128@gmail.com>
2026-06-04 15:16:54 +0900

    年と月のヘッダーを表示

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -4,12 +4,14 @@ const month = Number(process.argv[3]);
 const firstDay = new Date(year, month - 1, 1);
 const lastDay = new Date(year, month, 0);
 const weeks = ["日", "月", "火", "水", "木", "金", "土"];
-
-console.log(weeks.join(" "));
+const header = `${month}月 ${year}`;
+const dateWidth = 3;
+const calendarWidth = dateWidth * 7 - 1;
+const spaces = Math.floor((calendarWidth - header.length) / 2);
 
 let dates = "";
 for (let i = 0; i < firstDay.getDay(); i++) {
-  dates += "   ";
+  dates += " ".repeat(dateWidth);
 }
 for (let date = 1; date <= lastDay.getDate(); date++) {
   dates += `${String(date).padStart(2, " ")} `;
@@ -18,4 +20,6 @@ for (let date = 1; date <= lastDay.getDate(); date++) {
   }
 }
 
+console.log(`${" ".repeat(spaces)}${header}`);
+console.log(weeks.join(" "));
 console.log(dates);
